5b1ad154a2361991ce4aba5cea801461861de77e
sanjayi2000 <139948016+sanjayi2000@users.noreply.github.com>
2023-12-02 12:40:25 +0530

    Create PayrollSystem.java

---
+++ PayrollSystem.java
@@ -0,0 +1,54 @@
+// Employee class
+public class Employee {
+    private int employeeId;
+    private String employeeName;
+    private String designation;
+
+    public Employee(int employeeId, String employeeName, String designation) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.designation = designation;
+    }
+
+   
+public class HourlyEmployee extends Employee {
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public HourlyEmployee(int employeeId, String employeeName, String designation, double hourlyRate, int hoursWorked) {
+        super(employeeId, employeeName, designation);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+  
+}
+
+// SalariedEmployee class
+public class SalariedEmployee extends Employee {
+    private double monthlySalary;
+
+    public SalariedEmployee(int employeeId, String employeeName, String designation, double monthlySalary) {
+        super(employeeId, employeeName, designation);
+        this.monthlySalary = monthlySalary;
+    }
+
+    
+}
+
+// ExecutiveEmployee class
+public class ExecutiveEmployee extends SalariedEmployee {
+    private double bonusPercentage;
+
+    public ExecutiveEmployee(int employeeId, String employeeName, String designation, double monthlySalary, double bonusPercentage) {
+        super(employeeId, employeeName, designation, monthlySalary);
+        this.bonusPercentage = bonusPercentage;
+    }
+
+}
+
+// Main class
+public class PayrollSystem {
+    public static void main(String[] args) {
+    }
+}
